144a68be30130f81db5b1cd771e82d54380a114a
Tirthajyoti Ghosh <itirthahere@gmail.com>
2025-03-28 20:41:58 +0530

    refactor: Remove ExpoLlmMediapipeView export from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // Reexport the native module. On web, it will be resolved to ExpoLlmMediapipeModule.web.ts
 // and on native platforms to ExpoLlmMediapipeModule.ts
 export { default } from "./ExpoLlmMediapipeModule";
-export { default as ExpoLlmMediapipeView } from "./ExpoLlmMediapipeView";
 export * from "./ExpoLlmMediapipe.types";
 export {
   eventEmitter,
